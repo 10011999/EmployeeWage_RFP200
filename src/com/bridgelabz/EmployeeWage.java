c515bf969f22d2af62a734177509aa95094be207
10011999 <motewaraditya77@gmail.com>
2022-09-11 10:25:48 +0530

    Added Part Time Salary

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -5,13 +5,21 @@ public class EmployeeWage {
         System.out.println("Welcome Employees");
         int fullTime = 1;
         int perHourWage = 20;
-        double check = Math.floor(Math.random() * 10) % 2;
+        int partTime = 2;
+        double check = Math.floor(Math.random() * 10) % 3;
         if (check == fullTime) {
             System.out.println("Emp Present");
             int Hrs = 8;
             int salary = perHourWage * Hrs;
             System.out.println("Salary Amount: " +salary);
-        } else {
+        }
+        if (check == partTime){
+            System.out.println("Emp present in Part Time");
+            int Hrs = 8;
+            int salary = partTime * Hrs;
+            System.out.println("Part Time Salary " +salary);
+        }
+        else {
             System.out.println("Emp Absent");
             int Hrs = 0;
             int salary = perHourWage * Hrs;
