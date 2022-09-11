61172fd4d51a63df656b45cd42b8edcc5a9ebf2f
10011999 <motewaraditya77@gmail.com>
2022-09-11 10:11:34 +0530

    Added Calculate Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,11 +4,18 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome Employees");
         int fullTime = 1;
+        int perHourWage = 20;
         double check = Math.floor(Math.random() * 10) % 2;
         if (check == fullTime) {
             System.out.println("Emp Present");
+            int Hrs = 8;
+            int salary = perHourWage * Hrs;
+            System.out.println("Salary Amount: " +salary);
         } else {
             System.out.println("Emp Absent");
+            int Hrs = 0;
+            int salary = perHourWage * Hrs;
+            System.out.println("Salary Amount: " +salary);
         }
     }
 }
